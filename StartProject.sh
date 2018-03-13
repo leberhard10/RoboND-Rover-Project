779dbecdc7fff5f28a77b1d5216f5805c2e2c0a0
LEberhard <leberhard10@gmail.com>
2018-03-13 11:24:17 -0400

    Include a startup script for the jupyter notebook

---
+++ StartProject.sh
@@ -0,0 +1,13 @@
+#Starts up the robotics class notes
+#!/bin/bash
+set -e
+. activate RoboND
+if [ -z "$1" ]
+  then
+    jupyter notebook 
+elif [ "$1" == *".ipynb"* ]
+  then
+    jupyter notebook "$1"
+else
+    exec "$@"
+fi
